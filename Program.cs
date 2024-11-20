205be3e9f2e1fd1ffc6b940c5ca5a08cabf6f02c
William Cesar <willcesar010421@gmail.com>
2024-11-20 16:21:08 -0300

    feat: add swagger

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@ using PrimeiraApi.Rotas;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddCors(option => option
     .AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -14,6 +15,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
